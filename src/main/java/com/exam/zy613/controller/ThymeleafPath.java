6eb7bb018c9e05acdd40b3bd17dcc35ec6c46485
howie-huang <3151506606@qq.com>
2020-06-13 14:29:30 +0800

    提交人：howie-huang 原因：新建项目

---
+++ src/main/java/com/exam/zy613/controller/ThymeleafPath.java
@@ -0,0 +1,16 @@
+package com.exam.zy613.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * thymeleaf路径跳转
+ * @author 31515
+ */
+@Controller
+public class ThymeleafPath {
+    @RequestMapping("/hello")
+    public String toHello(){
+        return "hello";
+    }
+}
